54e0d39a81e3794ae873ef73538b89f68361e25a
Jefferson Rocha <58996149+JefRocha@users.noreply.github.com>
2025-11-23 21:27:49 -0300

    feat: inicializa o backend NestJS com filtro de exceção global e CORS.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
   console.info('Servidor Node pronto em http://localhost:3000');
 }
 bootstrap();
